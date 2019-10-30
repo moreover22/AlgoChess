8c3fb7cb5722f4b97f7558ea535c6b0a8d5c60d9
moreover22 <amore@fi.uba.ar>
2019-10-30 12:10:29 -0300

    primer commit

---
+++ src/test/java/fiuba/algo3/AlgoChess/AppTest.java
@@ -0,0 +1,20 @@
+package fiuba.algo3.AlgoChess;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
