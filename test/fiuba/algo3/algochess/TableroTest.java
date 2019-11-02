4a21e2ff5498d3f66399c5eae6baab805f728e3a
moreover22 <amore@fi.uba.ar>
2019-11-02 00:19:57 -0300

    Fixed JUnit error

--- test/fiuba/algo3/algochess/TableroTest.java
+++ test/fiuba/algo3/algochess/TableroTest.java
@@ -2,9 +2,12 @@ package fiuba.algo3.algochess;
 
 import fiuba.algo3.algochess.casillero.ColocarEnCasilleroEnemigoException;
 import fiuba.algo3.algochess.casillero.ColocarEnCasilleroOcupadoException;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TableroTest {
     @Test
