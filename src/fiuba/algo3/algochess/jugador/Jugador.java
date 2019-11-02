baa4b945178bd771a43dfd8f103b0d453c9f62cb
franco <franfusco99@gmail.com>
2019-11-02 20:46:15 -0300

    creacion de clase y test de jugador

---
+++ src/fiuba/algo3/algochess/jugador/Jugador.java
@@ -0,0 +1,24 @@
+package fiuba.algo3.algochess.jugador;
+
+import fiuba.algo3.algochess.Tablero;
+
+public class Jugador {
+    private int cantPuntos;
+    private char[] nombre;
+    private int cantPiezas;
+
+
+    public Jugador() {
+        this.cantPuntos = 20;
+        this.cantPiezas = 0;
+    }
+}
+
+public agregarUnidad(Tablero tablero,Pieza pieza,int x,int y){
+    public int PuntosActuales = this.cantPuntos;
+    this.cantPuntos = pieza DescontarCoste(PuntosActuales);
+    this.cantPiezas ++;
+
+    tablero colocarPieza(pieza,x,y);
+
+}
---
+++ test/fiuba/algo3/algochess/JugadorTest.java
@@ -0,0 +1,21 @@
+package fiuba.algo3.algochess;
+
+import fiuba.algo3.algochess.jugador.ExcesoDePuntosException;
+import fiuba.algo3.algochess.jugador.Jugador;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class JugadorTest {
+    @Test
+    public void NoSePuedeExcederDelMaximoDePuntosDeEntidades() throws ExcesoDePuntosException {
+        Tablero tablero = new Tablero();
+        Jugador jugador = new Jugador();
+
+
+
+    }
+}
