838aca42988ccefa64525e060dc8b22e4555b72d
Agustín More <amore@fi.uba.ar>
2019-11-06 12:28:01 -0300

    Update Curandero.java

--- src/fiuba/algo3/algochess/pieza/Curandero.java
+++ src/fiuba/algo3/algochess/pieza/Curandero.java
@@ -5,6 +5,6 @@ public class Curandero extends Pieza {
     public Curandero () {
         this.setCoste(2);
         this.setVida(75);
-        this.setHabilidad(new Curacion());
+        this.setHabilidad(new Curacion(15));
     }
 }
