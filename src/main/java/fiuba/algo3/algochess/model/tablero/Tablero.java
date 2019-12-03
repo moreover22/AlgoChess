136e3d5f78af11322f550d75139a21a380e70705
Agustín More <amore@fi.uba.ar>
2019-12-03 16:10:33 -0300

    Update Tablero.java

--- src/main/java/fiuba/algo3/algochess/model/tablero/Tablero.java
+++ src/main/java/fiuba/algo3/algochess/model/tablero/Tablero.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public class Tablero implements Aliable, Parseable {
