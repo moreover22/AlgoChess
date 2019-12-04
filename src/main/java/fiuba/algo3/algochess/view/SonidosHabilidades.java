42a470bbd7757e43e7c1d11cabc32d9ca0bc7150
franco <franfusco99@gmail.com>
2019-12-04 20:00:11 -0300

    agrego todos los sonidos menos el de espada pesada

--- src/main/java/fiuba/algo3/algochess/view/SonidosHabilidades.java
+++ src/main/java/fiuba/algo3/algochess/view/SonidosHabilidades.java
@@ -8,20 +8,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SonidosHabilidades {
-    private static final String pathToSonidoAtaqueSoldado = "sonido_ataque.mp3";
-    private static final String pathToSonidoAtaqueJinete = "";
-    private static final String pathToSonidoAtaqueCatapulta = "";
-    private static final String pathToSonidoAtaqueCurandero = "";
+    private static final String pathToSonidoAtaqueSoldado = "soldado.mp3";
+    private static final String pathToSonidoAtaqueJinete = "jinete.mp3";
+    private static final String pathToSonidoAtaqueCatapulta = "catapulta.mp3";
+    private static final String pathToSonidoAtaqueCurandero = "curandero.mp3";
+    private static final String pathToSonidoProyectil = "proyectil.mp3";
+    private static final String pathToSonidoFlecha = "flecha.mp3";
+    private static final String pathToSonidoEspadaLiviana = "espadaLiviana.mp3";
 
     private static final File archivoSoldado = new File(pathToSonidoAtaqueSoldado);
     private static final File archivoJinete = new File(pathToSonidoAtaqueJinete);
     private static final File archivoCatapulta = new File(pathToSonidoAtaqueCatapulta);
     private static final File archivoCurandero = new File(pathToSonidoAtaqueCurandero);
+    private static final File archivoProyectil = new File(pathToSonidoProyectil);
+    private static final File archivoFlecha = new File(pathToSonidoFlecha);
+    private static final File archivoEspadaLiviana = new File(pathToSonidoEspadaLiviana);
+
 
     private static final Media audioAtaqueSoldado = new Media(archivoSoldado.toURI().toString());
     private static final Media audioAtaqueJinete = new Media(archivoJinete.toURI().toString());
     private static final Media audioAtaqueCatapulta = new Media(archivoCatapulta.toURI().toString());
     private static final Media audioAtaqueCurandero = new Media(archivoCurandero.toURI().toString());
+    private static final Media audioGolpeProyectil = new Media(archivoProyectil.toURI().toString());
+    private static final Media audioGolpeFlecha = new Media(archivoFlecha.toURI().toString());
+    private static final Media audioGolpeEspadaLiviana = new Media(archivoEspadaLiviana.toURI().toString());
+
+
     private static Map<String,Media> sonidos;
 
     static {
@@ -31,6 +43,10 @@ public class SonidosHabilidades {
         sonidos.put("jinete", audioAtaqueJinete);
         sonidos.put("catapulta", audioAtaqueCatapulta);
         sonidos.put("curandero", audioAtaqueCurandero);
+        sonidos.put("proyectil", audioGolpeProyectil);
+        sonidos.put("flecha", audioGolpeFlecha);
+        sonidos.put("espadaLiviana", audioGolpeEspadaLiviana);
+
     }
 
     public static MediaPlayer getReproductor(String tipoPieza){
